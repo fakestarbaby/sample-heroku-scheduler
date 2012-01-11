5d72baaa32f9b315ce83e9d1a1221dd2511a3610
fakestarbaby <fakestarbaby@gmail.com>
2012-01-11 12:05:59 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHerokuScheduler::Application.config.secret_token = '4d33dee313f0514860d1f9db1fa9ca71af7a6b4621d489239d71265ed8eb6a7b340a494b83b8fe739c3369ba9f20f667dfbe82c968950cceba9d183f22682423'
